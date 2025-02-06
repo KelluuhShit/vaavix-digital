e34907309758c99e994042b3e2f60db577860f6e
KelluuhShit <kronoh60@gmail.com>
2025-02-06 21:48:58 +0300

    updated homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Header from "../components/Header";
-import coverImage from "../assets/signinbg.jpeg"; // Import the cover image
+import coverImage from "../assets/homeCover.jpg";
+import whyChooseUsImage from "../assets/whyChoose.jpg";
 import "../styles/HomeScreen.css";
 
 const services = [
-  { title: "Development Services", description: "We deliver cutting-edge software solutions tailored to your business needs, empowering you to innovate, scale, and stay ahead in a competitive digital landscape.", image: "path/to/image1.jpg" },
-  { title: "Cloud & IT Solutions", description: " Empower your business with secure, scalable, and efficient cloud and IT solutions designed to optimize performance and drive innovation.", image: "path/to/image2.jpg" },
-  { title: "Digital Marketing", description: "Empower your business with secure, scalable, and efficient cloud and IT solutions designed to optimize performance and drive innovation.", image: "path/to/image3.jpg" },
-  { title: "Branding & Design", description: "Craft a memorable brand identity and user centric designs that resonate with your audience and elevate your digital presence.", image: "path/to/image5.jpg" },
-  { title: "Content Creation", description: " Transform your ideas into captivating content that engages your audience and drives results. From immersive AR/VR experiences to compelling video marketing, we bring your vision to life.", image: "path/to/image6.jpg" },
-  { title: "Automation Services", description: "Streamline your operations and enhance efficiency with our cutting-edge automation solutions. From AI-powered chatbots to seamless employee onboarding, we simplify complex processes for your business.", image: "path/to/image7.jpg" },
-  { title: "Online Courses", description: "Upskill and stay ahead in your career with our industry-recognized online courses. Gain hands-on experience and earn certifications to validate your expertise.", image: "path/to/image8.jpg" },
-  { title: "Social Media Influencer", description: "Streamline your influencer marketing campaigns with our end-to-end platform. Find the right influencers, manage campaigns, and track performance with ease.", image: "path/to/image9.jpg" },
-  { title: "Social Media Manager", description: "Elevate your brand’s social media presence with our comprehensive management services. From strategy to execution, we handle it all to drive engagement and growth.", image: "path/to/image10.jpg" },
-  { title: "Hosting And Domains", description: "Reliable, secure, and scalable hosting and domain solutions to power your online presence. From registration to migration, we’ve got you covered.", image: "path/to/image4.jpg" },
+  { title: "Development Services", description: "We deliver cutting-edge software solutions tailored to your business needs, empowering you to innovate, scale, and stay ahead in a competitive digital landscape.", image: require("../assets/Vav_Logo.png") },
+  { title: "Cloud & IT Solutions", description: "Empower your business with secure, scalable, and efficient cloud and IT solutions designed to optimize performance and drive innovation.", image: require("../assets/Vav_Logo.png") },
+  { title: "Digital Marketing", description: "Empower your business with secure, scalable, and efficient cloud and IT solutions designed to optimize performance and drive innovation.", image: require("../assets/Vav_Logo.png") },
+  { title: "Branding & Design", description: "Craft a memorable brand identity and user centric designs that resonate with your audience and elevate your digital presence.", image: require("../assets/Vav_Logo.png") },
+  { title: "Content Creation", description: "Transform your ideas into captivating content that engages your audience and drives results. From immersive AR/VR experiences to compelling video marketing, we bring your vision to life.", image: require("../assets/Vav_Logo.png") },
+  { title: "Automation Services", description: "Streamline your operations and enhance efficiency with our cutting-edge automation solutions. From AI-powered chatbots to seamless employee onboarding, we simplify complex processes for your business.", image: require("../assets/Vav_Logo.png") },
+  { title: "Online Courses", description: "Upskill and stay ahead in your career with our industry-recognized online courses. Gain hands-on experience and earn certifications to validate your expertise.", image: require("../assets/Vav_Logo.png") },
+  { title: "Social Media Influencer", description: "Streamline your influencer marketing campaigns with our end-to-end platform. Find the right influencers, manage campaigns, and track performance with ease.", image: require("../assets/Vav_Logo.png") },
+  { title: "Social Media Manager", description: "Elevate your brand’s social media presence with our comprehensive management services. From strategy to execution, we handle it all to drive engagement and growth.", image: require("../assets/Vav_Logo.png") },
+  { title: "Hosting And Domains", description: "Reliable, secure, and scalable hosting and domain solutions to power your online presence. From registration to migration, we’ve got you covered.", image: require("../assets/Vav_Logo.png") },
 ];
 
 const Home = () => {
@@ -22,6 +23,7 @@ const Home = () => {
       <Header />
       <div className="cover-image-container">
         <img src={coverImage} alt="Cover" className="cover-image" />
+        <div className="cover-overlay"></div>
         <div className="cover-content">
           <h1> Innovate, Engage, 
           Succeed</h1>
@@ -53,8 +55,31 @@ const Home = () => {
                 <button className="explore-service-btn">Explore Service →</button>
               </div>
             ))}
-          </div>
+      </div>
+
+      <div className="why-choose-us">
+            <div className="why-choose-us-left">
+              <h1>
+                Why Choose Us for Your Development Needs?
+              </h1>
+
+              <div>
+                <p>✓ Experienced and skilled development team</p>
+                <p>✓ Agile and transparent development process</p>
+                <p>✓ Client-centric approach</p>
+                <p>✓ On-time delivery and competitive pricing</p>
+                <p>✓ Proven track record of successful projects</p>
+              </div>
+
+            </div>
+            <div className="why-choose-us-right">
+              <img src={whyChooseUsImage} alt="Why Choose Us" className="why-choose-us-image" />
+            </div>
+      </div>
+
     </div>
+
+      
   );
 };
 
